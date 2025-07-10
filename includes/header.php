6373f49465866321c851265c4dddc1eeace3108f
KIIZA TREVOUR <kiizatrevour33@gmail.com>
2025-07-10 13:02:11 +0300

    changed header file

--- includes/header.php
+++ includes/header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Employee Management</title>
+    <title>Employees Management</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
@@ -19,7 +19,7 @@
                     <a class="nav-link" href="index.php">Home</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="create.php">Add Employee</a>
+                    <a class="nav-link" href="create.php">Add Employees</a>
                 </li>
             </ul>
         </div>
